54fe3cd1c833f7748b42d79158be337899a83d8f
0xRektora <13756173+0xRektora@users.noreply.github.com>
2023-12-05 11:57:13 -0500

    chore: deleted deprecated contracts

--- contracts/deprecated/balancer/interfaces/IBalancerPool.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.18;
-
-interface IBalancerPool {
-    function getRate() external view returns (uint256);
-
-    function balanceOf(address _user) external view returns (uint256);
-}
