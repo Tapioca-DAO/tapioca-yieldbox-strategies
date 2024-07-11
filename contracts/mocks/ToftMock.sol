22efc13276fbcad279fb0606521c5854fb80edf0
cryptotechmaker <grigore.cosmin@gmail.com>
2024-07-11 09:17:35 +0300

    feat(`stargate v2 strategy`): added `StargateV2Strategy` [`86du2qrnp`]

--- contracts/mocks/ToftMock.sol
+++ contracts/mocks/ToftMock.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.22;
 
 import {ERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Pearlmit, IPearlmit, PearlmitHash} from "tapioca-periph/pearlmit/Pearlmit.sol";
 
 /*
 ████████╗ █████╗ ██████╗ ██╗ ██████╗  ██████╗ █████╗ 
@@ -14,19 +15,29 @@ import {ERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ToftMock is ERC20 {
     address public erc20;
+    IPearlmit public pearlmit;
+
+    error FailedToWrap();
 
     constructor(address erc20_, string memory name_, string memory symbol_) ERC20(name_, symbol_) {
         erc20 = erc20_;
     }
 
+    function setPearlmit(IPearlmit _pearlmit) external {
+        pearlmit = _pearlmit;
+    }
+
     function wrap(address from, address to, uint256 amount) external payable returns (uint256 minted) {
-        IERC20(erc20).transferFrom(from, address(this), amount);
+        bool isErr = pearlmit.transferFromERC20(from, address(this), erc20, amount);
+        if (isErr) revert FailedToWrap();
+
         _mint(to, amount);
         return amount;
     }
 
-    function unwrap(address to, uint256 amount) external {
+    function unwrap(address to, uint256 amount) external returns (uint256 unwrapped) {
         _burn(msg.sender, amount);
         IERC20(erc20).transfer(to, amount);
+        return amount;
     }
 }
